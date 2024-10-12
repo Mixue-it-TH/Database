bd2f0e2572552953b8f1208fe515a27097efd261
Xsmitylnwza <111405516+Xsmitylnwza@users.noreply.github.com>
2024-10-12 15:21:24 +0700

    update for PBI25+

--- scripts/08_insertscript_version3.sql
+++ scripts/08_insertscript_version3.sql
@@ -1,14 +1,17 @@
 INSERT INTO `itbkanban3`.`status` (`id`,`statusName`,`statusColor`,`statusDescription`) VALUES
 (1,'No Status','#6b7280','A status has not been assigned'),
 (2,'To Do','#F5C330','The task is included in the project'),
-(3,'Doing','#F5C330','The task is being worked on'),
-(4,'Done','#30F558','The task has been completed');
+(3,'In Progress','#F5C330','The task is being worked on'),
+(4,'Reviewing','#F55D30','The task is being reviewed'),
+(5,'Testing','#30F558','The task is being tested'),
+(6,'Waiting','#60F558','The task is waiting for a resource'),
+(7,'Done','#30F558','The task has been completed');
 
 insert into `itbkanban3`.`users` (`oid`, `userName`,`email`) values("1",'test', 'test@mail.com');
 insert into `itbkanban3`.`config` (`noOfTasks`,`limitMaximumTask`) values(5, 0);
-insert into `itbkanban3`.`boards` (`boardId`,`boardName`,`configId`) values('TEST011','test2', 15);
+insert into `itbkanban3`.`boards` (`boardId`,`boardName`,`configId`) values('TEST011','test2', 1);
 
-insert into `itbkanban3`.`boarduser` (`userId`,`boardId`,`role`) values("1","TEST011","ONWER");
+insert into `itbkanban3`.`collaborator` (`userId`,`boardId`,`role`,`ownerName`) values("1","TEST011","OWNER","I'm a OWNER");
 
 insert into `itbkanban3`.`customStatus` (`statusId`, `statusName`, `statusDescription`, `statusColor`, `boardId`) values
 (1,'No Status','A status has not been assigned','#6b7280',"TEST011");
